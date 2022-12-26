7c255f85c71dacb03022a0d58c1951de4843ebfd
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-26 22:01:32 +0100

    add stock

---
+++ script/stock.js
@@ -0,0 +1,65 @@
+// Use strict 
+    'use strice';
+
+slogObj = {
+    name: 'slog',
+    logo: './assets/images/Logo-primary.png',
+    info: {
+        place: 'DrammenVeien',
+        city: 'Drammen',
+        cityCode: '3060',
+
+        email: 'post@slog.frontendkenterik.no',
+        phone: '450 00 000',
+    },
+    
+
+    product: {
+        [0]: {
+            id: 1,
+            name: '',
+            short_description: '',
+            description: '',
+            price: '',
+            onstock: true, 
+            stock: 10,
+
+        },
+        [1]: {
+            id: 2, 
+            name: '',
+            short_description: '',
+            description: '',
+            price: '',
+            onstock: true,
+            stock: 15,
+        },
+        [2]: {
+            id: 3,
+            name: '',
+            short_description: '',
+            description: '',
+            price: '',
+            onstock: false,
+            stock: 1,
+        },
+        [3]: {
+            id: 4,
+            name: '',
+            short_description: '',
+            description: '',
+            price: '',
+            onstock: true,
+            stock: 2,
+        },
+        [4]: {
+            id: 5,
+            name: '',
+            short_description: '',
+            description: '',
+            price: '',
+            onstock: false,
+            stock: 100,
+        }
+    }
+}
