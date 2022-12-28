9170b39996f9f0c546173f0a6783f9eed7f0809d
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-28 17:25:47 +0100

    adding api keys

---
+++ script/base/base.js
@@ -0,0 +1,16 @@
+// Using stric
+'using strict';
+// DOM Tree 
+    // Header 
+        // nav
+            const nav = document.querySelector('#nav');
+        // nav Desktop Version
+            const navDesktop = document.querySelector('.nav__desktop');
+        // nav Mobil Version
+            const navMobil = document.querySelector('.nav__mobil');
+
+// Starting conditions
+
+    // Cart
+        let itemCart = []
+// API 
