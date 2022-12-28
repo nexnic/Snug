e24608e33f58ee185acc7f382a3cf5659a31ceb0
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-28 17:28:21 +0100

    update api

--- script/base/base.js
+++ script/base/base.js
@@ -13,4 +13,15 @@
 
     // Cart
         let itemCart = []
-// API 
+// API
+    // URL 
+        const URL = 'https://frontendkenterik.no/slugapi/wp-json/wc/v3'
+    // API key
+        // Consumer key
+            const consumerKey = 'ck_8f55f41cea38bcdce6b841f8b574f843ed50c8a0';
+        // Consumer Secret 
+            const consumerSecret = 'cs_678bce6da556ca86c5357207a0359e2f4200fe0c';
+    // End Point
+        // Product
+            const  epProduct = '/products';
+    
