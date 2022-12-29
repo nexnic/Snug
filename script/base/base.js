e27e171a4e2bf126918cc1243776d721702b36b3
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-29 10:57:36 +0100

    adding product to js

--- script/base/base.js
+++ script/base/base.js
@@ -8,11 +8,24 @@
             const navDesktop = document.querySelector('.nav__desktop');
         // nav Mobil Version
             const navMobil = document.querySelector('.nav__mobil');
-
+    // Main 
+        // Product
+            const product = document.querySelector('product');
+        // product Text
+            const producttxt = document.querySelector('product__txt');
+        // product Content
+            const productcontent = document.querySelector('product__content');
 // Starting conditions
-
+    // Check api call
+        const startInterval = setInterval(CheckInterval, 3000);
+    // API call
+        let APICall = false;
+    // Object Copy
+        let objectCopy = false;
     // Cart
         let itemCart = []
+    // Product Object 
+        let snugObj
 // API
     // URL 
         const URL = 'https://frontendkenterik.no/slugapi/wp-json/wc/v3'
@@ -21,7 +34,32 @@
             const consumerKey = 'ck_8f55f41cea38bcdce6b841f8b574f843ed50c8a0';
         // Consumer Secret 
             const consumerSecret = 'cs_678bce6da556ca86c5357207a0359e2f4200fe0c';
+        // auth
+            let auth = bAuth(consumerKey, consumerSecret);
     // End Point
         // Product
             const  epProduct = '/products';
     
+
+// Auto
+function bAuth(key, secret) {
+    let hash = btoa(key + ':' + secret);
+        return 'Basic ' + hash;
+}
+
+// Checking API Call 
+function CheckInterval() {
+    console.log('checking call')
+    if(APICall === true && objectCopy === true) {
+        // Need to make a clear interval
+        console.log('complett')
+        clearinterval(startInterval);
+    }
+}
+
+// Clear Interval 
+    // Value1 Is the function 
+    function clearinterval(value1) {
+        clearInterval(value1);
+    }
+
